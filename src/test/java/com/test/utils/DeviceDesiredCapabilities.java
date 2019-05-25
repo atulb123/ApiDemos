260ac4a91db0223e5f9223d291ab6854e44b6516
unknown <abharadwajj@gmail.com>
2019-05-25 18:13:10 +0530

    ee

--- src/test/java/com/test/utils/DeviceDesiredCapabilities.java
+++ src/test/java/com/test/utils/DeviceDesiredCapabilities.java
@@ -2,6 +2,7 @@ package com.test.utils;
 
 import java.io.FileReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
@@ -9,7 +10,15 @@ public class DeviceDesiredCapabilities {
 	@SuppressWarnings("unchecked")
 	public static Map<String, String> getDesiredCapabilities(String DeviceName) throws Exception {
 		Gson gson = new Gson();
-		return (Map<String, String>) Arrays.asList(gson.fromJson(
-				new FileReader(System.getProperty("user.dir") + "/Capabilities/DeviceCapabilities.json"),Object[].class)).stream().filter(e->e.toString().contains(DeviceName)).findFirst().get();
+		Object[] ee = gson.fromJson(
+				new FileReader(System.getProperty("user.dir") + "/Capabilities/DeviceCapabilities.json"),Object[].class);
+		for(Object o:ee)
+		{
+			if(o.toString().contains(DeviceName))
+			{
+				return (Map<String, String>) o;
+			}
+		}
+		return null;
 	}
 }
